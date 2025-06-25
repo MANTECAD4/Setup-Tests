8c2339763c71d91a24e138b30009967af7e449bb
MANTECAD4 <98615675+MANTECAD4@users.noreply.github.com>
2025-06-24 22:16:50 -0600

    Update setup-tests.js

--- setup-tests.js
+++ setup-tests.js
@@ -1,30 +1,30 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
+const fs = require("fs");
+const { execSync } = require("child_process");
 
-console.log('Downloading dependences...');
+console.log("Downloading dependences...");
 execSync(
-	'npm install --save-dev ' +
-		[
-			'@babel/core',
-			'@babel/preset-env',
-			'@babel/preset-react',
-			'@babel/preset-typescript',
-			'@testing-library/react',
-			'@testing-library/jest-dom@latest',
-			'@types/jest',
-			'babel-jest',
-			'isomorphic-fetch',
-			'jest',
-			'jest-environment-jsdom',
-		].join(' '),
-	{ stdio: 'inherit' }
+  "npm install --save-dev " +
+    [
+      "@babel/core",
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "@babel/preset-typescript",
+      "@testing-library/react",
+      "@testing-library/jest-dom@latest",
+      "@types/jest",
+      "babel-jest",
+      "isomorphic-fetch",
+      "jest",
+      "jest-environment-jsdom",
+    ].join(" "),
+  { stdio: "inherit" }
 );
 
-console.log('Creating files...');
+console.log("Creating files...");
 
 fs.writeFileSync(
-	'babel.config.cjs',
-	`
+  "babel.config.cjs",
+  `
 module.exports = {
     presets: [
         [ '@babel/preset-env', { targets: { esmodules: true } } ],
@@ -36,21 +36,24 @@ module.exports = {
 );
 
 fs.writeFileSync(
-	'jest.config.cjs',
-	`
+  "jest.config.cjs",
+  `
 module.exports = {
-    presets: [
-        [ '@babel/preset-env', { targets: { esmodules: true } } ],
-        [ '@babel/preset-react', { runtime: 'automatic' } ],
-        '@babel/preset-typescript',
-    ],
+	transform: {
+		'^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+	},
+	testEnvironment: 'jest-environment-jsdom',
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+	transformIgnorePatterns: ['/node_modules/(?!query-string)/'],
 };
+
 `.trimStart()
 );
 
 fs.writeFileSync(
-	'jest.setup.js',
-	`
+  "jest.setup.js",
+  `
 import '@testing-library/jest-dom';
 import 'isomorphic-fetch'; // Optional
 import { TextEncoder, TextDecoder } from 'util';
@@ -61,21 +64,20 @@ global.TextDecoder = TextDecoder;
 `.trimStart()
 );
 
-console.log('Config files created uwu ✅');
+console.log("Config files created uwu ✅");
 
-const packageJsonPath = './package.json';
+const packageJsonPath = "./package.json";
 
 // Leer y parsear el package.json
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 
 // Agregar o modificar un script personalizado
 packageJson.scripts = {
-	...packageJson.scripts,
-	test: 'jest',
-	'test:watch': 'jest --watchAll',
+  ...packageJson.scripts,
+  test: "jest --watchAll",
 };
 
 // Guardar los cambios en package.json
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 
-console.log('Scripts added to package.json ✅');
+console.log("Scripts added to package.json ✅");
